2480ff5e498de53124d0fbe7504ef5c6a8dc9073
XbaiMiaoAPI <3104026189@qq.com>
2020-08-05 13:18:54 +0800

    格式化代码

--- src/com/xbaimiao/Main.java
+++ src/com/xbaimiao/Main.java
@@ -1,8 +1,10 @@
 package com.xbaimiao;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
     private static Main instance;
     public static Main plugin(){
@@ -13,6 +15,13 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         instance = this;
+        Bukkit.getPluginManager().registerEvents(this,this);
     }
+//
+//    @EventHandler
+//    public void test(@NotNull PlayerChatEvent event){
+//        Bungees.connect(event.getPlayer(),"miaodalu");
+//        System.out.println("正在将" +event.getPlayer().getName() +"传送至miaodalu");
+//    }
 
 }
